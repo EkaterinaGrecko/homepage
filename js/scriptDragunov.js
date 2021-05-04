5746430236285866ac871fc00f5c3e491a2eb1c0
Ekaterina Grecko <eagrecko@gmail.com>
2021-05-04 20:29:16 +0200

    refactored js by functions

--- js/scriptDragunov.js
+++ js/scriptDragunov.js
@@ -1,10 +1,23 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Cześć!");
+    }
 
 
-let button = document.querySelector(".section__buttonPhotos");
-let images = document.querySelector(".image__gallery");
+    const toggleShowHideImages = () => {
+        const button = document.querySelector(".js-ShowHidePhotosButton");
+        const images = document.querySelector(".js-images");
+        button.innerText = images.classList.toggle("image__gallery--hidden") ? "Pokaż zdjęcia" : "Ukryj zdjęcia";
+    };
 
-button.addEventListener("click", () => {
-    button.innerText = images.classList.toggle("image__hidden") ? "Pokaż zdjęcia" : "Ukryj zdjęcia";
-});
 
+    const init = () => {
+        const button = document.querySelector(".js-ShowHidePhotosButton");
+        button.addEventListener("click", toggleShowHideImages);
+    };
+
+    init();
+
+    welcome();
+
+}
